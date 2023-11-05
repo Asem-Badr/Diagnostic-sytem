c3470340f53ef456358656499a805adcbd092b01
AsemBadr <asembadr200@gmail.com>
2023-11-05 13:47:31 +0200

    documenting watch_dog.py

--- watch_dog.py
+++ watch_dog.py
@@ -6,9 +6,12 @@ file_path = "./data.json"  # Replace with the path to your file
 last_modified_time = os.path.getmtime(file_path)
 
 while True:
-    time.sleep(1)
+    #getmtime system call returns the lastest modification time of the file
     current_modified_time = os.path.getmtime(file_path)
+    #compare the time of the last modification with the current modification time
     if current_modified_time != last_modified_time:
         print(f'File {file_path} has been modified')
-        subprocess.call(["python3", "publish.py"])  # Replace with the path to your script
+        #if the json file content is update call the publish the data through mqtt
+        subprocess.call(["python3", "publish.py"]) 
         last_modified_time = current_modified_time
+    time.sleep(1)
